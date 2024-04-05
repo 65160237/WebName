30e11c2343f1e5511b9cc4b73753a3ee7a43a520
65160237 <152171373+65160237@users.noreply.github.com>
2024-04-05 18:53:26 +0700

    save

--- scriptTest.js
+++ scriptTest.js
@@ -3,3 +3,16 @@ const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sidebarItems = document.querySelectorAll('.sidebar-item');
+
+    sidebarItems.forEach(item => {
+        item.addEventListener('click', function() {
+            sidebarItems.forEach(innerItem => {
+                innerItem.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    });
+});
